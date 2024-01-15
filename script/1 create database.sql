0ef099e9528863b2ef334006fad9f667f8d16f88
Nur Inayah <150806284+slothly23@users.noreply.github.com>
2024-01-15 21:43:01 +0700

    query membuat tabel category

--- script/1 create database.sql
+++ script/1 create database.sql
@@ -1,1 +1,9 @@
+-- membuat database
 create database toko_online_db;
+
+-- membuat tabel Category
+create table Category(
+    id int not null primary key auto_increment,
+    name varchar(50) not null
+);
+
