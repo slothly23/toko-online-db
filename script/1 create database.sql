da3c1c98ad17653ca1b0b66c95aa99efcf8b1277
Nur Inayah <150806284+slothly23@users.noreply.github.com>
2024-01-15 22:01:34 +0700

    menambahkan 10 data kategori

--- script/1 create database.sql
+++ script/1 create database.sql
@@ -7,3 +7,24 @@ create table Category(
     name varchar(50) not null
 );
 
+-- menambahkan 10 data kategori ke tabel Category
+insert into category (name)
+values ('Elektronik');
+insert into category (name)
+values ('Sepatu');
+insert into category (name)
+values ('Perlengkapan Rumah');
+insert into category (name)
+values ('Pakaian Perempuan');
+insert into category (name)
+values ('Aksesori');
+insert into category (name)
+values ('Jam Tangan');
+insert into category (name)
+values ('Otomotif');
+insert into category (name)
+values ('Kesehatan');
+insert into category (name)
+values ('Pakaian Bayi');
+insert into category (name)
+values ('Tas');
